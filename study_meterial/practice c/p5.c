2b0f547b2b194525174a84b8a19416d82a184ee0
alnajasalih <alnajasalih@gmail.com>
2026-02-11 13:50:27 +0530

    new

---
+++ study_meterial/practice c/p5.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+
+
+void main()
+{
+    #include<stdio.h>
+
+
+void main()
+{
+   int a,b,c,d;
+   float answ;
+   scanf("%d%d%d%d",&a,&b,&c,&d);
+   if((c-d)!=0)
+   {
+       answ=(a+b)/(float)(c-d);
+       printf("ratio=%.2f",answ);
+
+   }
+   else
+   {
+       printf("Division by 0");
+   }
+
+
+
+}
